26278300b97efa4c1704c2a34dff1720229b5d42
David <david.pok@gmail.com>
2020-07-09 18:22:18 -0700

    adjusts projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Grow, makeStyles, Typography } from "@material-ui/core/";
+import { Waypoint } from "react-waypoint";
+
+const useStyles = makeStyles((theme) => ({
+  projects: {
+    paddingLeft: "23%",
+    paddingRight: "23%",
+    marginTop: 150,
+    marginBottom: 150,
+  },
+}));
 
 export default function Projects() {
-    return (
-        <div className="projects">
-            <h1>THIS THE PROJECTS SECTION</h1>
-        </div>
-    )
+  const [animate1, setAnimate1] = useState(false);
+  const [animate2, setAnimate2] = useState(false);
+  const classes = useStyles();
+
+  return (
+    <div className="projects">
+      <div className={classes.projects}>
+        <Waypoint onEnter={() => setAnimate1(true)}>
+          <Grow in={animate1} timeout={650}>
+            <Typography variant="h3" align="left">
+              A couple of my projects:
+            </Typography>
+          </Grow>
+        </Waypoint>
+      </div>
+    </div>
+  );
 }
